5c06c6b338e75d4c96d4139f8d6052fb600d0913
smolpotatoes <126000905+smolpotatoes@users.noreply.github.com>
2023-04-11 12:19:50 +0100

    Consolidating sites into root

--- homepage-script.js
+++ homepage-script.js
@@ -1,30 +1,32 @@
+alert("This site uses cookies because they are delicious!");
+
 let suggestions = [
-    "Channel",
-    "CodingLab",
-    "CodingNepal",
-    "YouTube",
-    "YouTuber",
-    "YouTube Channel",
-    "Blogger",
-    "Bollywood",
-    "Vlogger",
-    "Vechiles",
-    "Facebook",
-    "Freelancer",
-    "Facebook Page",
-    "Designer",
-    "Developer",
-    "Web Designer",
-    "Web Developer",
-    "Login Form in HTML & CSS",
-    "How to learn HTML & CSS",
-    "How to learn JavaScript",
-    "How to became Freelancer",
-    "How to became Web Designer",
-    "How to start Gaming Channel",
-    "How to start YouTube Channel",
-    "What does HTML stands for?",
-    "What does CSS stands for?",
+  "rug",
+  "purple",
+  "red",
+  "yellow",
+  "blue",
+  "green",
+  "pink",
+  "black",
+  "Vlogger",
+  "Vechiles",
+  "Facebook",
+  "Freelancer",
+  "Facebook Page",
+  "Designer",
+  "Developer",
+  "Web Designer",
+  "Web Developer",
+  "Login Form in HTML & CSS",
+  "How to learn HTML & CSS",
+  "How to learn JavaScript",
+  "How to became Freelancer",
+  "How to became Web Designer",
+  "How to start Gaming Channel",
+  "How to start YouTube Channel",
+  "What does HTML stands for?",
+  "What does CSS stands for?",
 ];
 
 // getting all required elements
@@ -36,39 +38,39 @@ let linkTag = searchInput.querySelector("a");
 let webLink;
 
 // if user press any key and release
-input.onkeyup = (e)=>{
-    let userData = e.target.value; //user enetered data
-    let emptyArray = [];
-    if(userData){
-        emptyArray = suggestions.filter((data)=>{
-            //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
-            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase()); 
-        });
-        emptyArray = emptyArray.map((data)=>{
-            // passing return data inside li tag
-            return data = '<li>'+ data +'</li>';
-        });
-        searchInput.classList.add("active"); //show autocomplete box
-        showSuggestions(emptyArray);
-        let allList = resultBox.querySelectorAll("li");
-        for (let i = 0; i < allList.length; i++) {
-            //adding onclick attribute in all li tag
-            allList[i].setAttribute("onclick", "select(this)");
-        }
-    }else{
-        searchInput.classList.remove("active"); //hide autocomplete box
+input.onkeyup = (e) => {
+  let userData = e.target.value; //user enetered data
+  let emptyArray = [];
+  if (userData) {
+    emptyArray = suggestions.filter((data) => {
+      //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
+      return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+    });
+    emptyArray = emptyArray.map((data) => {
+      // passing return data inside li tag
+      return (data = "<li>" + data + "</li>");
+    });
+    searchInput.classList.add("active"); //show autocomplete box
+    showSuggestions(emptyArray);
+    let allList = resultBox.querySelectorAll("li");
+    for (let i = 0; i < allList.length; i++) {
+      //adding onclick attribute in all li tag
+      allList[i].setAttribute("onclick", "select(this)");
     }
-}
+  } else {
+    searchInput.classList.remove("active"); //hide autocomplete box
+  }
+};
 
-function showSuggestions(list){
-    let listData;
-    if(!list.length){
-        userValue = inputBox.value;
-        listData = '<li>'+ userValue +'</li>';
-    }else{
-        listData = list.join('');
-    }
-    resultBox.innerHTML = listData;
+function showSuggestions(list) {
+  let listData;
+  if (!list.length) {
+    userValue = inputBox.value;
+    listData = "<li>" + userValue + "</li>";
+  } else {
+    listData = list.join("");
+  }
+  resultBox.innerHTML = listData;
 }
 
 $('#carouselExample').on('slide.bs.carousel', function (e) {
